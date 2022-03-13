9dfbe7c136c3b3b80a585a39bb3c120f1aef6116
Yanivv77 <Yanivv77@gmail.com>
2022-03-14 00:49:42 +0200

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <Router>
       <Header />
       <main className="py-3">
-        <Container></Container>
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+        </Container>
       </main>
+      <Footer />
     </Router>
   )
 }
