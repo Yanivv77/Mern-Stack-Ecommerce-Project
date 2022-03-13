4874162bf2c3371d0365a8a4a86ae616217742c3
Yanivv77 <Yanivv77@gmail.com>
2022-03-14 01:15:26 +0200

    Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './components/HomeScreen'
+import HomeScreen from './Screens/HomeScreen'
 
 const App = () => {
   return (
--- frontend/src/components/HomeScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const HomeScreen = ({ match }) => {
-  return <></>
-}
-
-export default HomeScreen
