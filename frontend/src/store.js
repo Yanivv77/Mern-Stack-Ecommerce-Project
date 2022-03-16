59652a01dd89d04ad597a29c122a68ded0a23376
Yanivv77 <Yanivv77@gmail.com>
2022-03-16 19:49:36 +0200

    fix

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-
-const reducer = combineReducers({})
-
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
-
-const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
-
-const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
-
-const initialState = {
-  cart: {
-    cartItems: cartItemsFromStorage,
-    shippingAddress: shippingAddressFromStorage,
-  },
-  userLogin: { userInfo: userInfoFromStorage },
-}
-
-const middleware = [thunk]
-
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
-
+const store = createStore(composeWithDevTools(applyMiddleware()))
 export default store
