ae8a3e7efa7e5435cfe80dfbd62f28d5328d7281
Yanivv77 <Yanivv77@gmail.com>
2022-03-16 11:49:17 +0200

    rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -16,7 +17,9 @@ const Product = ({ product }) => {
           </Card.Title>
         </Link>
 
-        <Card.Text as="div"></Card.Text>
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
