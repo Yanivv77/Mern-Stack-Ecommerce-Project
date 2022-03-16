4f164c6f198ac79cc328f7243c706f6feeda9469
Yanivv77 <Yanivv77@gmail.com>
2022-03-16 19:03:31 +0200

    MongoDB Connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,9 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
+    const conn = await mongoose.connect(process.env.MONGO_URI)
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(`MongoDB Connected: ${conn.connection.host.dim}`.cyan)
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
