bd3d173aa0d85b2a85f1e94e13791deaa878aafb
Yanivv77 <Yanivv77@gmail.com>
2022-03-16 21:31:55 +0200

    import data into db

--- backend/seeder.js
+++ backend/seeder.js
@@ -5,7 +5,7 @@ import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
 import Product from './models/productModel.js'
-import Order from './models/orderModel.js'
+
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -14,7 +14,6 @@ connectDB()
 
 const importData = async () => {
   try {
-    await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
@@ -38,7 +37,6 @@ const importData = async () => {
 
 const destroyData = async () => {
   try {
-    await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
