a39bdccdd669cff18e79cd0a88726014f5031225
Yanivv77 <Yanivv77@gmail.com>
2022-03-16 13:24:18 +0200

    Message & loader components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './Screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} exact />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
