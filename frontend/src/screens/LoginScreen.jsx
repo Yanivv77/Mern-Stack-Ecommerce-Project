7f90b1c3b27dc6032fc6921bdda689a83e4ec18d
Yanivv77 <Yanivv77@gmail.com>
2022-03-17 12:31:15 +0200

    LoginScreen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function LoginScreen() {
-  return <div>LoginScreen</div>
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import FormContainer from '../components/FormContainer'
+
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      <Form>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>New Customer?</Col>
+      </Row>
+    </FormContainer>
+  )
 }
 
 export default LoginScreen
