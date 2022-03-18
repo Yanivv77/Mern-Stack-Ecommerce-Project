ee32e70fde9a1520a3eddf53953d132366982dfd
Yanivv77 <Yanivv77@gmail.com>
2022-03-18 11:46:06 +0200

    Reverting to 969b683

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({
-  productList: productListReducer,
-})
+const reducer = combineReducers({})
 
 const initialState = {}
 
