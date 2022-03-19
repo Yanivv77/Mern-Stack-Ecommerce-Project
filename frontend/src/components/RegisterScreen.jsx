69938cfb2a17b14278ae83f85bf2dcce979cbba9
Yanivv77 <Yanivv77@gmail.com>
2022-03-19 18:19:58 +0200

    RegisterScreen

--- frontend/src/components/RegisterScreen.jsx
+++ frontend/src/components/RegisterScreen.jsx
@@ -80,7 +80,7 @@ const RegisterScreen = ({ location, history }) => {
 
       <Row className="py-3">
         <Col>
-          Have an Account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
+          Have an Account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login </Link>
         </Col>
       </Row>
     </FormContainer>
