ff9ded6dd3e57f35817ed2447c7fd1920a34887b
Yanivv77 <Yanivv77@gmail.com>
2022-06-09 04:29:11 +0300

    seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -5,7 +5,7 @@ import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
 import Product from './models/productModel.js'
-
+import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -14,6 +14,7 @@ connectDB()
 
 const importData = async () => {
   try {
+    await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
@@ -37,6 +38,7 @@ const importData = async () => {
 
 const destroyData = async () => {
   try {
+    await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -22,6 +25,12 @@ app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
@@ -29,7 +38,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....')
@@ -41,4 +52,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
