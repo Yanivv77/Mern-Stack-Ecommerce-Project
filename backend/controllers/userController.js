8ce6d815330a39f7d6805573397508ef918a7c6c
Yanivv77 <Yanivv77@gmail.com>
2022-06-09 04:20:23 +0300

    Controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -144,6 +144,7 @@ const getUserById = asyncHandler(async (req, res) => {
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private/Admin
+
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id)
 
